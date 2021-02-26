5ad333c6fcbec5bc7ff0befd794ea8160a05931b
morteza roozbehi <mr.roz16126@gmail.com>
2021-02-26 06:38:39 +0330

    add first get routes in folder v1 of api

---
+++ api/v1/routes/product.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+    /////////////////////////////////////////
+
+router.get('/', (req, res) => {
+    res.send('GET request to the product ...')
+})
+
+
+router.get('/:id', (req, res) => {
+    const data = {
+        msg: 'response',
+        prof: req.params.id
+    }
+    res.status(200).json({
+        status: "good !",
+        data: data
+    })
+})
+
+
+
+module.exports = router
---
+++ app.js
@@ -0,0 +1,29 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+    //////////////////////////////////////////////
+    //////////////////////////////////////
+const productRoutes = require('./api/v1/routes/product')
+
+///////////////////////////////////////
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/product', productRoutes)
+
+
+
+
+/////////////////////////// not found
+app.use((req, res, next) => {
+    res.status(404).json({
+        msg: "not found rout"
+    })
+
+})
+
+
+
+
+
+app.listen(3000, console.log('running ...'))
