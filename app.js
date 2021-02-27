21ce026f81607279b1533a79c68fb0f22f80114b
morteza roozbehi <mr.roz16126@gmail.com>
2021-02-27 08:52:26 +0330

    add pack cors for security api from different ports

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const { options } = require('./api/v1/routes/product')
 const app = express()
+var cors = require('cors')
     //////////////////////////////////////////////
     //////////////////////////////////////
 const productRoutes = require('./api/v1/routes/product')
@@ -9,6 +11,19 @@ const productRoutes = require('./api/v1/routes/product')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Headers', '*')
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', '*')
+//         return res.status(200).json({}).next()
+//     }
+
+// })
+app.use(cors())
+
+
 
 app.use('/product', productRoutes)
 
